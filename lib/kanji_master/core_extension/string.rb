a9b67fb8b172c1aa8c31f1a5a83e5caddbf87a5b
DONGSHIH TENG <tou9702@gmail.com>
2017-06-06 17:56:17 +0900

    Add core extension for string

---
+++ lib/kanji_master/core_extension/string.rb
@@ -0,0 +1,3 @@
+require "kanji_master/string_extension"
+
+String.send(:include, ::KanjiMaster::StringExtension)
---
+++ lib/kanji_master/string_extension.rb
@@ -0,0 +1,16 @@
+module KanjiMaster
+  module StringExtension
+
+    def katakana; kana end
+    def kana
+      converter = Converter.new
+      converter.kana(self)
+    end
+
+    def hiragana; hira end
+    def hira
+      converter = Converter.new
+      converter.hira(self)
+    end
+  end
+end
