8a1b820dcaf88ee8c2de62e40c3e6d64ef8a1838
DONGSHIH TENG <tou9702@gmail.com>
2017-06-06 21:28:44 +0900

    Refactor

--- lib/kanji_master/random.rb
+++ lib/kanji_master/random.rb
@@ -7,5 +7,10 @@ module KanjiMaster
     def prefecture_name
       Prefecture.data[rand(1..47)][:name]
     end
+
+    def number_kanji; chinese_number_character end
+    def chinese_number_character
+      Constant::KANJI_NUMBER[rand(0..9)]
+    end
   end
 end
--- spec/kanji_master/random_spec.rb
+++ spec/kanji_master/random_spec.rb
@@ -5,16 +5,25 @@ describe KanjiMaster::Random do
     described_class.new
   end
 
-  it 'it should return random prefecture_hash' do
-    random_prefecture_hash = instance.prefecture_hash
+  1.upto(47*3) do |i|
+    it "should return random prefecture_hash (#{i})" do
+      random_prefecture_hash = instance.prefecture_hash
 
-    expect(random_prefecture_hash.keys).to eq [:name, :romaji, :hiragana, :katakana, :area]
-    random_prefecture_hash.values.each do |value|
-      expect(value).not_to be nil
+      expect(random_prefecture_hash.keys).to eq [:name, :romaji, :hiragana, :katakana, :area]
+      random_prefecture_hash.values.each do |value|
+        expect(value).not_to be nil
+      end
+    end
+
+    it 'should return random prefecture_name' do
+      expect(instance.prefecture_name).not_to be nil
     end
   end
 
-  it 'it should return random prefecture_name' do
-    expect(instance.prefecture_name).not_to be nil
+  1.upto(10*3) do |i|
+    it "should random number kanji (#{i})" do
+      expect(instance.chinese_number_character).not_to be nil
+      expect(instance.number_kanji).not_to be nil
+    end
   end
 end
