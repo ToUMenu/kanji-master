b25d36792aa42a35e98c709134c67642e4f3f3c8
DONGSHIH TENG <tou9702@gmail.com>
2017-06-06 11:27:13 +0900

    Add alias katakana command

--- lib/kanji_master/converter.rb
+++ lib/kanji_master/converter.rb
@@ -2,12 +2,13 @@ require 'nkf'
 
 module KanjiMaster
   class Converter
-    attr_accessor :api_key, :kana
+    attr_accessor :api_key, :kana, :katakana
 
     def initialize(api_key: nil)
       @api_key = api_key
     end
 
+    def katakana(text); kana(text) end
     def kana(text)
       return nil unless text.match(RegexConstant::HIRAGANA_REGEX)
       NKF.nkf('-w --katakana', text)
--- spec/kanji_master/converter_spec.rb
+++ spec/kanji_master/converter_spec.rb
@@ -5,21 +5,25 @@ describe KanjiMaster::Converter do
     described_class.new()
   end
 
-  describe 'kana method' do
+  describe 'kana/katakana method' do
     it 'should convert to kana (1)' do
       expect(instance.kana('こんにちは')).to eq "コンニチハ"
+      expect(instance.katakana('こんにちは')).to eq "コンニチハ"
     end
 
     it 'should convert to kana (2)' do
       expect(instance.kana('すごいですよ')).to eq "スゴイデスヨ"
+      expect(instance.katakana('すごいですよ')).to eq "スゴイデスヨ"
     end
 
     it 'should return nil if argument is not hiragana (1)' do
       expect(instance.kana('alphabet')).to eq nil
+      expect(instance.katakana('alphabet')).to eq "nil"
     end
 
     it 'should return nil if argument is not hiragana (2)' do
       expect(instance.kana('漢字')).to eq nil
+      expect(instance.katakana('漢字')).to eq "nil"
     end
   end
 end
